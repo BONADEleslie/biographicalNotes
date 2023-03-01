a8c32395fdea0200bab1fc01f10e85f3ed719754
kevin <13022071029@163.com>
2023-03-01 21:40:30 +0800

    主题

---
+++ src/components/button/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <el-button>按钮</el-button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "kevin-button",
+  };
+  </script>
+  
+  
---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: kevin
+ * @Date: 2023-03-01 20:00:00
+ * @LastEditors: kevin
+ * @LastEditTime: 2023-03-01 20:00:00
+ * @Description: 基础组件库
+ */
+
+// 路径在 webpack.config.js 配置
+import kButton from 'com/button';
+
+export default {
+    kButton
+}
